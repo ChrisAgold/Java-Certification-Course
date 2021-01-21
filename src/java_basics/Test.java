20839bacabb3499a7bca26052c5931b69b138ff0
Chris Agold <christophersagold@gmail.com>
2021-01-20 18:37:32 -0600

    completed variables notes

--- src/java_basics/Test.java
+++
@@ -1,7 +0,0 @@
-package java_basics;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello the weather is great!");
-    }
-}
---
+++ src/java_basics/Variables.java
@@ -0,0 +1,16 @@
+package java_basics;
+// Data Type Syntax
+public class Variables {
+    public static void main(String[] args) {
+        // Numbers
+        int x = 34;
+        System.out.println(x);
+        System.out.println(x + 4);
+        // Strings
+        String words = "This is a string";
+        System.out.println(words);
+        // append to string
+        System.out.println(words + " these are some more words.");
+
+    }
+}
