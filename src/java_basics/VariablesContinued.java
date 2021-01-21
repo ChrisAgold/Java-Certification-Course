576d4902dccb5098e750cf111508a72cd94d4ad6
Chris Agold <christophersagold@gmail.com>
2021-01-20 18:49:21 -0600

    added data type notes

--- src/java_basics/VariablesContinued.java
+++ src/java_basics/VariablesContinued.java
@@ -1,7 +1,21 @@
 package java_basics;
-
+// Primitive Data Types
 public class VariablesContinued {
     public static void main(String[] args) {
-
+        // int data type
+        int num = 100000000;
+        System.out.println(num);
+        // Long data type, holds larger numbers
+        long bigNumber = 10000000000000L;
+        // short data type, can only hold 32767
+        short shortNum = 32767;
+        // byte, supports 127 maximum
+        byte reallySmallNumber = -128;
+        // double data type
+        double decimalVariable = 394.003;
+        // true or false
+        boolean decision = false;
+        // single character data type
+        char letter = 'a';
     }
 }
