998506bca9ea8fd524ee85000721f6c577957871
Chris Agold <christophersagold@gmail.com>
2021-02-18 18:00:00 -0600

    started car dealership example

---
+++ src/car_dealership/Dealership.java
@@ -0,0 +1,23 @@
+package car_dealership;
+
+public class Dealership {
+    public static void main(String[] args) {
+        Customer cust1 = new Customer();
+        cust1.setName("Tom");
+        cust1.setAddress("123 Anthing St. ");
+        cust1.setCashOnHand(12000);
+
+        cust1.purchaseCar(vehicle, emp, false);
+
+//         handleCustomer( Customer cust, boolean finance, Vehicle vehicle)
+//         if(finance == true)
+//         runCreditHistory(Customer cust, double doubleAmount)
+//         else if (vehicle.getPrice() <= cust.getCashOnHand():
+//         processTransaction(Customer cust, Vehicle vehicle)
+//         else:
+//         tell customer to bring more money
+
+    }
+
+
+}
